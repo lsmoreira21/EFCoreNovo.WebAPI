781f4f54da9266d6094fad6857ea3964204adce5
Luciano dos Santos Moreira <luciano.moreira@ms.senac.br>
2020-06-22 16:40:27 -0400

    Adicionar arquivos de projeto.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCore.Dominio;
+using EFCore.Repo;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreNovo.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public readonly HeroiContext _context;
+
+        public ValuesController(HeroiContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/values
+        [HttpGet("filtro/{nome}")]
+        public ActionResult<IEnumerable<string>> GetFiltro(string nome)
+        {
+            var listHeroi = _context.Herois
+                .Where(h => h.Nome.Contains(nome))
+                .ToList();
+            //var listHeroi = (from heroi in _context.Herois
+            //                 where heroi.Nome.Contains(nome)
+            //                 select heroi).ToList();
+            return Ok(listHeroi);
+        }
+
+        // GET api/values/5
+        [HttpGet("Atualizar/{nameHero}")]
+        public ActionResult Get(string nameHero)
+        {
+            // var heroi = new Heroi { Nome = nameHero };
+
+            var heroi = _context.Herois
+                            .Where(h => h.Id == 1)
+                            .FirstOrDefault();
+
+            heroi.Nome = "Homem Aranha";
+
+            //_context.Herois.Add(heroi);
+            // Contexto.Add(heroi);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        // GET api/values/5
+        [HttpGet("AddRange")]
+        public ActionResult GetAddRange()
+        {
+            _context.AddRange(
+                new Heroi { Nome = "Capital America"},
+                new Heroi { Nome = "Doutor Estranho" },
+                new Heroi { Nome = "Pantera Negra" },
+                new Heroi { Nome = "Viúva Negra" },
+                new Heroi { Nome = "Hulk" },
+                new Heroi { Nome = "Gavião Arqueiro" },
+                new Heroi { Nome = "Capitã Marvel" }
+
+                );
+
+
+            var heroi = _context.Herois
+                            .Where(h => h.Id == 1)
+                            .FirstOrDefault();
+
+            heroi.Nome = "Homem Aranha";
+
+            //_context.Herois.Add(heroi);
+            // Contexto.Add(heroi);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpGet("Delete/{id}")]
+        public void Delete(int id)
+        {
+            var heroi = _context.Herois
+                                .Where(X => X.Id == id)
+                                .Single();
+            _context.Herois.Remove(heroi);
+            _context.SaveChanges();
+        }
+    }
+}
